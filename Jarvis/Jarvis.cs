47e7854d41ad0f204714944a4241f4a080304946
Jacob Christensen <jacob.h.christensen@gmail.com>
2016-07-23 21:47:06 -0600

    Added unhandled exception handler

--- Jarvis/Jarvis.cs
+++ Jarvis/Jarvis.cs
@@ -1,16 +1,18 @@
 ﻿using System.Threading;
+using System.IO;
 
 namespace Jarvis
 {
   using System;
   using Nancy.Hosting.Self;
 
-  class Jarvis
+  public class Jarvis
   {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-      var uri =
-          new Uri("http://localhost:8080");
+      AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler (ExceptionHandler);
+
+      var uri = new Uri("http://localhost:8080");
 
       var config = new HostConfiguration();
       config.UrlReservations.CreateAutomatically = true;
@@ -23,6 +25,20 @@ namespace Jarvis
         Console.WriteLine("Press any [Enter] to close Jarvis.");
         Console.ReadLine();
       }     
+    }
+
+    // Catches all unhandled exceptions and logs them to a file
+    public static void ExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+    {
+      Exception e = (Exception)args.ExceptionObject;
+      StreamWriter writer = new StreamWriter ("jarvisExceptionLog.txt", true);
+      writer.WriteLine ("-------------------------------");
+      writer.WriteLine (DateTime.Now.ToString ());
+      writer.WriteLine (e.TargetSite);
+      writer.WriteLine (e.Message);
+      writer.WriteLine (e.StackTrace);
+      writer.Flush ();
+      writer.Close ();
     }
   }
 }
