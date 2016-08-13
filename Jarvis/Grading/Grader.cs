34e85e963e1a60973348cc0ab5cabcdb28dc2c6f
Jacob Christensen <jacob.h.christensen@gmail.com>
2016-08-13 10:29:55 -0600

    Add 60 second timeout

--- Jarvis/Grading/Grader.cs
+++ Jarvis/Grading/Grader.cs
@@ -3,30 +3,32 @@ using System.Diagnostics;
 using System.Text;
 using HtmlDiff;
 using System.IO;
+using System.Timers;
+using System.Threading;
 
 namespace Jarvis
 {
   public class Grader
   {
-    public Grader ()
+    public Grader()
     {
 
     }
 
     public GradingResult Grade(Assignment homework)
     {
-      GradingResult result = new GradingResult ();
+      GradingResult result = new GradingResult();
 
       // Style check
       result.StyleMessage = StyleCheck(homework);
 
       // Compile
-      result.CompileMessage = Compile (homework);     
+      result.CompileMessage = Compile(homework);
 
       // Run tests
-      if (result.CompileMessage == "Success!!") 
+      if (result.CompileMessage == "Success!!")
       {
-        result.OutputMessage = RunProgram (homework);
+        result.OutputMessage = RunProgram(homework);
 
         result.CorrectOutput = result.OutputMessage.Contains("No difference");
       }
@@ -43,40 +45,40 @@ namespace Jarvis
 
     private void RecordResult(Assignment homework, GradingResult result)
     {
-      string timestamp = DateTime.Now.ToString ();
+      string timestamp = DateTime.Now.ToString();
 
-      StreamWriter writer = new StreamWriter (homework.Path + "results.txt", true);
-      writer.WriteLine (timestamp + " " + homework.StudentId + " " + result.Grade); 
-      writer.Flush ();
-      writer.Close ();      
+      StreamWriter writer = new StreamWriter(homework.Path + "results.txt", true);
+      writer.WriteLine(timestamp + " " + homework.StudentId + " " + result.Grade);
+      writer.Flush();
+      writer.Close();
     }
 
 
-    private string StyleCheck (Assignment homework)
+    private string StyleCheck(Assignment homework)
     {
-      Process p = new Process ();
-      
+      Process p = new Process();
+
       p.StartInfo.UseShellExecute = false;
       p.StartInfo.RedirectStandardOutput = true;
       p.StartInfo.RedirectStandardError = true;
 
-      string styleExe = Jarvis.Config.AppSettings.Settings ["styleExe"].Value;
+      string styleExe = Jarvis.Config.AppSettings.Settings["styleExe"].Value;
       p.StartInfo.FileName = styleExe;
       p.StartInfo.Arguments = homework.FullName;
-      p.Start ();
-      
-      string result = p.StandardError.ReadToEnd ();
-      result = result.Replace (homework.Path, "");
-      result = result.Replace (" ", "&nbsp;");
+      p.Start();
+
+      string result = p.StandardError.ReadToEnd();
+      result = result.Replace(homework.Path, "");
+      result = result.Replace(" ", "&nbsp;");
       result = result.Replace("\n", "<br />");
-      p.WaitForExit ();
-      
+      p.WaitForExit();
+
       return result;
     }
 
     private string Compile(Assignment homework)
     {
-      Process p = new Process ();
+      Process p = new Process();
 
       p.StartInfo.UseShellExecute = false;
       p.StartInfo.RedirectStandardOutput = true;
@@ -84,20 +86,23 @@ namespace Jarvis
 
       p.StartInfo.FileName = "g++";
       p.StartInfo.Arguments = homework.FullName + " -o" + homework.Path + homework.StudentId;
-      p.Start ();
+      p.Start();
 
-      string result = p.StandardError.ReadToEnd ();
-      result = result.Replace (homework.Path, "");
-      result = ToHtmlEncoding (result);
+      string result = p.StandardError.ReadToEnd();
+      result = result.Replace(homework.Path, "");
+      result = ToHtmlEncoding(result);
 
-      p.WaitForExit ();
+      p.WaitForExit();
 
-      return (!string.IsNullOrEmpty (result)) ? result : "Success!!";
+      return (!string.IsNullOrEmpty(result)) ? result : "Success!!";
     }
 
-    private string RunProgram (Assignment homework)
+    private string RunProgram(Assignment homework)
     {
-      Process p = new Process ();
+      Timer hangCheckingTimer = new Timer();
+      hangCheckingTimer.Elapsed += HangCheckingTimer_Elapsed;
+
+      Process p = new Process();
 
       p.StartInfo.UseShellExecute = false;
       p.StartInfo.RedirectStandardOutput = true;
@@ -106,61 +111,82 @@ namespace Jarvis
 
       p.StartInfo.FileName = homework.Path + homework.StudentId;
       //p.StartInfo.Arguments = file;
-      p.Start ();
+      p.Start();
 
       if (File.Exists(homework.Path + "input.txt"))
       {
-        StreamReader reader = new StreamReader (homework.Path + "input.txt");
+        StreamReader reader = new StreamReader(homework.Path + "input.txt");
 
         while (!reader.EndOfStream)
-        {          
-          p.StandardInput.WriteLine (reader.ReadLine ());
+        {
+          p.StandardInput.WriteLine(reader.ReadLine());
         }
       }
-      string actualOutput = p.StandardOutput.ReadToEnd ();
 
-      p.WaitForExit ();
+      string actualOutput = p.StandardOutput.ReadToEnd();
+      int timeout = 60; // seconds
+
+      while (!p.HasExited && timeout > 0)
+      {
+        timeout--;
+        Thread.Sleep(1000); // 1 second sleep
+      }
+
+      if (!p.HasExited)
+      {
+        p.Kill();
+      }
+      else
+      {
+        p.WaitForExit();
+      }
+      p.Dispose();
 
       StringBuilder result = new StringBuilder();
-      string expectedOutput = GetExpectedOutput (homework);
+      string expectedOutput = GetExpectedOutput(homework);
 
-      result.AppendLine ("<h3>Actual</h3>");
-      result.AppendLine ("<p>" + ToHtmlEncoding(actualOutput) + "</p>");
-      result.AppendLine ("<br />");
-      result.AppendLine ("<h3>Expected</h3>");
-      result.AppendLine ("<p>" + ToHtmlEncoding(expectedOutput) + "</p>");
-      result.AppendLine ("<br />");
-      result.AppendLine ("<h3>Diff</h3>");
+      result.AppendLine("<h3>Actual</h3>");
+      result.AppendLine("<p>" + ToHtmlEncoding(actualOutput) + "</p>");
+      result.AppendLine("<br />");
+      result.AppendLine("<h3>Expected</h3>");
+      result.AppendLine("<p>" + ToHtmlEncoding(expectedOutput) + "</p>");
+      result.AppendLine("<br />");
+      result.AppendLine("<h3>Diff</h3>");
 
       string testDiff = string.Empty;
 
-      if (actualOutput.Equals (expectedOutput, StringComparison.Ordinal)) 
+      if (actualOutput.Equals(expectedOutput, StringComparison.Ordinal))
       {
         testDiff = "No difference";
       }
       else
       {
-        testDiff = HtmlDiff.HtmlDiff.Execute (actualOutput, expectedOutput);
+        testDiff = HtmlDiff.HtmlDiff.Execute(actualOutput, expectedOutput);
       }
 
-      result.AppendLine ("<p>" + testDiff + "</p>");
+      result.AppendLine("<p>" + testDiff + "</p>");
 
       // Don't leave binaries hanging around
-      File.Delete (homework.Path + homework.StudentId);
+      File.Delete(homework.Path + homework.StudentId);
 
-      return !string.IsNullOrEmpty (actualOutput) ? result.ToString() : "No output...";
+      return !string.IsNullOrEmpty(actualOutput) ? result.ToString() : "No output...";
+    }
+
+    private void HangCheckingTimer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+      throw new NotImplementedException();
     }
 
     private string GetExpectedOutput(Assignment homework)
     {
-      StreamReader reader = new StreamReader (homework.Path + "output.txt");
+      StreamReader reader = new StreamReader(homework.Path + "output.txt");
 
-      return reader.ReadToEnd ();
+      return reader.ReadToEnd();
     }
 
     private string ToHtmlEncoding(string text)
     {
-      text = text.Replace (" ", "&nbsp;");
+      text = text.Replace(" ", "&nbsp;");
       text = text.Replace("\n", "<br />");
 
       return text;
